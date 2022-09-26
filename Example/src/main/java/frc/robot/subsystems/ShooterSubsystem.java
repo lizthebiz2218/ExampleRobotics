2c7bd81bd330d64893259492d4d6046f1c93a340
lizthebiz2218 <113944920+lizthebiz2218@users.noreply.github.com>
2022-09-26 16:55:37 -0400

    Update ShooterSubsystem.java
    you're welcome jordan

--- Example/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ Example/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,9 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ShooterSubsystem() {}
 
   @Override
   public void periodic() {
