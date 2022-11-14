b9fd6405a2833a9ddd39024058b5f1a7676d21e0
lizthebiz2218 <113944920+lizthebiz2218@users.noreply.github.com>
2022-11-14 17:29:14 -0500

    everything interesting we've done
    This is the stuff Jordan and I have done

--- Example/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ Example/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,12 +4,47 @@
 // Lizzie Jordan
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//this is what was copied from Jor Jor's computer
+//this was edited
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import stuff, inclduing TalonFx
+
+// add values 
+// - motor.set...  (speed),(jotstick,values), getcoder[current position] 
+// e.g: given the start and end position how can you make it always go to one point and stay at one point?
+// You can make the speed decrease while applying the fact the distance from the start and end is also decreasing
+ //Creates a new ExampleSubsystem.//
 public class ShooterSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
+
+  private final TalonFX shooterMotor1 = new TalonFX(//which physical motor it is);
+  private final TalonFX shooterMotor2 = new TalonFX(//which physical motor it is);
+
   public ShooterSubsystem() {
-    
+  
+  }
+  
+  private double speed = 0;
+
+  
+  //public void getShooterSpeed(currentShooterSpeed, double speed) {
+    //int currentSpeed = 50;
+    //if (currentShooterSpeed < speed) {
+      //float differentialSpeed = (speed - currentShooterSpeed)/100;
+      //make difference a percentage
+      //increase current speed by difference
+      //shooterMotor1.set(ControlMode.PercentOutput, differentialSpeed)
+      
+//    }
+//  }
+
+  public void shoot(double speed) {
+
+    this.speed = speed;
+
+    shooterMotor1.set(ControlMode.Velocity, speed)
+    shooterMotor2.set(ControlMode.Velocity, speed)
+
   }
 
   @Override
@@ -22,3 +57,5 @@ public class ShooterSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run during simulation
   }
 }
+
+//Elizabeth and Jordan
